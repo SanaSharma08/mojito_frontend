d67fb0be424a85ea8477d24c5024a4eb7dd542b3
Sana Sharma <sanasharma2808@gmail.com>
2026-05-20 20:05:31 +0530

    Hero Section & Cocktails Section

---
+++ constants/index.js
@@ -0,0 +1,183 @@
+const navLinks = [
+ {
+	id: "cocktails",
+	title: "Cocktails",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+const cocktailLists = [
+  {
+    name: "Royal Rajput Reserve",
+    state: "Rajasthan",
+    detail: "Bottle",
+    price: "₹670",
+  },
+  {
+    name: "Goan Midnight Spirit",
+    state: "Goa",
+    detail: "Bottle",
+    price: "₹1,000",
+  },
+  {
+    name: "Himalayan Malt Brew",
+    state: "Himachal",
+    detail: "750 ml",
+    price: "₹500",
+  },
+  {
+    name: "Punjab Oak Barrel",
+    state: "Punjab",
+    detail: "600 ml",
+    price: "₹585",
+  },
+];
+
+const mockTailLists = [
+  {
+    name: "Aam Saffron Splash",
+    state: "Maharashtra",
+    detail: "Bottle",
+    price: "₹420",
+  },
+  {
+    name: "Mint Masala Mojito",
+    state: "Delhi",
+    detail: "Bottle",
+    price: "₹500",
+  },
+  {
+    name: "Kokum Citrus Cooler",
+    state: "Goa",
+    detail: "750 ml",
+    price: "₹335",
+  },
+  {
+    name: "Rose Cardamom Fizz",
+    state: "Kashmir",
+    detail: "600 ml",
+    price: "₹420",
+  },
+];
+
+const profileLists = [
+ {
+	imgPath: "/images/profile1.png",
+ },
+ {
+	imgPath: "/images/profile2.png",
+ },
+ {
+	imgPath: "/images/profile3.png",
+ },
+ {
+	imgPath: "/images/profile4.png",
+ },
+];
+
+const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
+const storeInfo = {
+ heading: "Where to Find Us",
+ address: "456, Raq Blvd. #404, Los Angeles, CA 90210",
+ contact: {
+	phone: "(555) 987-6543",
+	email: "hello@jsmcocktail.com",
+ },
+};
+
+const openingHours = [
+ { day: "Mon–Thu", time: "11:00am – 12am" },
+ { day: "Fri", time: "11:00am – 2am" },
+ { day: "Sat", time: "9:00am – 2am" },
+ { day: "Sun", time: "9:00am – 1am" },
+];
+
+const socials = [
+ {
+	name: "Instagram",
+	icon: "/images/insta.png",
+	url: "#",
+ },
+ {
+	name: "X (Twitter)",
+	icon: "/images/x.png",
+	url: "#",
+ },
+ {
+	name: "Facebook",
+	icon: "/images/fb.png",
+	url: "#",
+ },
+];
+
+const sliderLists = [
+ {
+	id: 1,
+	name: "Classic Mojito",
+	image: "/images/drink1.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 2,
+	name: "Raspberry Mojito",
+	image: "/images/drink2.png",
+	title: "A Zesty Classic That Never Fails",
+	description:
+	 "The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
+ },
+ {
+	id: 3,
+	name: "Violet Breeze",
+	image: "/images/drink3.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 4,
+	name: "Curacao Mojito",
+	image: "/images/drink4.png",
+	title: "Crafted With Care, Poured With Love",
+	description:
+	 "Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
+ },
+];
+
+export {
+ navLinks,
+ cocktailLists,
+ mockTailLists,
+ profileLists,
+ featureLists,
+ goodLists,
+ openingHours,
+ storeInfo,
+ socials,
+ sliderLists,
+};
